efa31675f2760142c9c965a9cca447d7b2cbb58f
CoderKevinZhang <zhangkai@oregonstate.edu>
2019-09-10 16:37:26 -0700

    updated

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-content-body">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
